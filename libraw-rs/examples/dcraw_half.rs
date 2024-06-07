182c8bd527aceea8b5fc22c2b84ce55f05cb2935
Saul <47524932+spector-9@users.noreply.github.com>
2024-06-07 18:19:46 +0530

    Fix: Add value for options in rawparams

--- libraw-rs/examples/dcraw_half.rs
+++ libraw-rs/examples/dcraw_half.rs
@@ -4,12 +4,12 @@ use std::path::Path;
 pub fn main() -> anyhow::Result<()> {
     for arg in std::env::args().skip(1) {
         let mut p = Processor::builder()
-            .with_raw_params([RawParams::UseDngSdk(2)])
+            .with_raw_params([RawParams::UseDngSdk(2), RawParams::Options((1<< 1 | 1 << 21))])
             .with_params([Params::HalfSize(false)])
             .build();
         //let mut p = libraw_r::defaults::half_size();
         p.open(&arg)?;
-        dbg!(p.params());
+        dbg!(p.rawparams());
         println!(
             "Processing {arg} ({}, {})",
             p.idata().make.as_ascii(),
